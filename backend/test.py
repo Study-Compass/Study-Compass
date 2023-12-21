94ce23e9f05771fa8e917ddf1d4caa89a4ce2e42
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2023-12-21 14:18:00 -0500

    hello

---
+++ backend/test.py
@@ -0,0 +1,23 @@
+import json
+import re
+
+def process_json(file_path):
+    # Initialize an empty set
+    processed_names = set()
+
+    # Open and load the JSON file
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+        # Process each object in the JSON array
+        for item in data:
+            name = item
+            # Remove numbers and whitespace
+            clean_name = re.sub(r'[\d\s]', '', name)
+            processed_names.add(clean_name)
+
+    return processed_names
+
+# Replace 'classes.json' with the path to your JSON file
+result = process_json('classes.json')
+print(result)
