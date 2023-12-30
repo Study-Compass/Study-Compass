dc30b25e8323dacfd23a8effd96a1bdbb410c4a8
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2023-12-30 17:15:28 -0500

    added routing, needs fixing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,116 +1,16 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import './App.css';
-import Calendar from './components/Calendar/Calendar';
+import Room from './pages/Room';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-    const days = ["S", "M", "T", "W", "R", "F"];
-    const data = {
-        "name": "Academy Hall AUD",
-        "weekly_schedule": {
-            "M": [
-                {
-                    "class_name": "ENGR 1100",
-                    "start_time": "08:00",
-                    "end_time": "10:00"
-                },
-                {
-                    "class_name": "ENGR 1100",
-                    "start_time": "10:00",
-                    "end_time": "12:00"
-                },
-                {
-                    "class_name": "ENGR 1100",
-                    "start_time": "12:00",
-                    "end_time": "14:00"
-                },
-                {
-                    "class_name": "ENGR 1100",
-                    "start_time": "14:00",
-                    "end_time": "16:00"
-                },
-                {
-                    "class_name": "PSYC 4500",
-                    "start_time": "18:00",
-                    "end_time": "20:00"
-                }
-            ],
-            "T": [
-                {
-                    "class_name": "ADMN 1824",
-                    "start_time": "16:00",
-                    "end_time": "17:00"
-                },
-                {
-                    "class_name": "CHME 4040",
-                    "start_time": "13:00",
-                    "end_time": "14:00"
-                },
-                {
-                    "class_name": "ENGR 1100",
-                    "start_time": "10:30",
-                    "end_time": "12:30"
-                }
-            ],
-            "W": [
-                {
-                    "class_name": "CHME 4040",
-                    "start_time": "13:00",
-                    "end_time": "14:00"
-                },
-                {
-                    "class_name": "CHEM 2120",
-                    "start_time": "14:00",
-                    "end_time": "15:00"
-                }
-            ],
-            "R": [
-                {
-                    "class_name": "ENGR 1100",
-                    "start_time": "08:00",
-                    "end_time": "10:00"
-                },
-                {
-                    "class_name": "ENGR 1100",
-                    "start_time": "10:00",
-                    "end_time": "12:00"
-                },
-                {
-                    "class_name": "ENGR 1100",
-                    "start_time": "12:00",
-                    "end_time": "14:00"
-                },
-                {
-                    "class_name": "ENGR 1100",
-                    "start_time": "14:00",
-                    "end_time": "16:00"
-                },
-                {
-                    "class_name": "PSYC 4500",
-                    "start_time": "18:00",
-                    "end_time": "20:00"
-                }
-            ],
-            "F": [
-                {
-                    "class_name": "CHME 4040",
-                    "start_time": "13:00",
-                    "end_time": "14:00"
-                },
-                {
-                    "class_name": "ENGR 1100",
-                    "start_time": "10:00",
-                    "end_time": "12:00"
-                }
-            ]
-        }
-    }
-
-    const eventColors = useRef(new Map()).current;
-
     return (
-        <div className="App">
-            <Calendar data={data}/>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/room/:roomid" element={<Room />}/>
+            </Routes>
+        </Router>
+
     );
 }
 
