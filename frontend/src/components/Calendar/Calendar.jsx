406c8270195e73af145b10e1068f0845c0c1443d
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2023-12-31 16:12:18 -0500

    added concurrently, run with npm start in root

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -1,10 +1,30 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './Calendar.css';
 import DayColumn from '../DayColumn/DayColumn';
 
-function Calendar({data}){
+function Calendar({className}){
     const days = ["S", "M", "T", "W", "R", "F"];
     const eventColors = useRef(new Map()).current;
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`/getroom/${className}`);
+                const data = await response.json();
+                setData(data);
+            } catch (error) {
+                console.error("Error fetching data: ", error);
+            }
+        };
+
+        fetchData();
+    }, [className]);
+
+    // Render loading state while data is being fetched
+    if (!data) {
+        return <div>Loading...</div>;
+    }
 
     return (
             <div className="Calendar">
