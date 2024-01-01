d64570f8984c4bff6ee5925589fcb6b0d2dbf2bf
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2024-01-01 17:24:44 -0500

    added loading animatino

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -4,8 +4,17 @@ import DayColumn from '../DayColumn/DayColumn';
 
 function Calendar({className}){
     const days = ["S", "M", "T", "W", "R", "F"];
+    const loadColors = useRef(new Map()).current;
     const eventColors = useRef(new Map()).current;
     const [data, setData] = useState(null);
+
+    const load = [                
+        {
+            "class_name": "loading",
+            "start_time": "7:00",
+            "end_time": "20:00"
+        }
+    ];
 
     useEffect(() => {
         const fetchData = async () => {
@@ -23,7 +32,22 @@ function Calendar({className}){
 
     // Render loading state while data is being fetched
     if (!data) {
-        return <div>Loading...</div>;
+        return (
+            <div className="Calendar">
+                <div className="Calendar-header">
+                    <p>monday</p>
+                    <p>tuesday</p>
+                    <p>wednesday</p>
+                    <p>thursday</p>
+                    <p>friday</p>
+                </div>
+                <div className="Time-grid">
+                    {days.map((day) => (
+                        <DayColumn day={day} dayEvents={load} eventColors={loadColors} />
+                    ))}
+                </div>
+            </div>
+        );
     }
 
     return (
