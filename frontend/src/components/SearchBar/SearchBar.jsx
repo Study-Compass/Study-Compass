cbf255fb3a3d1620ebf65434f24e686245abf0a1
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2024-01-06 21:47:10 -0500

    added search enter functionality

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './SearchBar.css';
 import x from '../../assets/x.svg';
 
@@ -6,12 +6,25 @@ import x from '../../assets/x.svg';
 function SearchBar({ data, onEnter}) {
     const [searchInput, setSearchInput] = useState('');
     const [results, setResults] = useState([]);
+    const [lower, setLower] = useState("")
 
     const [isFocused, setIsFocused] = useState(false);
     const [selected, setSelected] = useState(0);
 
+    const inputRef = useRef(null);
+
     useEffect(() => {
-    }, [isFocused]);
+        if(searchInput === "" || results[0]==="no results found"){
+            setLower("");
+        }else if(results.length>0){
+
+            if(results[0]!="no results found"){
+                setLower(results[0].toLowerCase())
+            }
+        }
+    }, [results, searchInput]);
+
+
     const handleInputChange = (event) => {
         const value = event.target.value.toLowerCase();
         setSearchInput(value);
@@ -47,26 +60,30 @@ function SearchBar({ data, onEnter}) {
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
             if (results.length > 0) {
-                // setSearchInput(results[selected].toLowerCase());
-                // setResults([]);
-                // onEnter(results[selected]);
                 next(results[selected]);
+                inputRef.current.blur();
             }
         }
-        if (event.key === 'ArrowDown') {
+        if (event.key === 'ArrowDown' || event.key === 'ArrowRight') {
             if(selected === results.length-1){
                 setSelected(0);
             } else {
                 setSelected(selected+1);
             }
         }   
-        if (event.key === 'ArrowUp') {
+        if (event.key === 'ArrowUp' || event.key === 'ArrowLeft') {
             if(selected === 0){
                 setSelected(results.length-1);
             } else {
                 setSelected(selected-1);
             }
         }   
+        if (event.key === "Tab"){
+            if (results.length > 0 && results[0] != "no results found"){
+                event.preventDefault();
+                setSearchInput(results[0].toLowerCase())
+            }
+        }
     };
 
     function click(event){
@@ -91,6 +108,11 @@ function SearchBar({ data, onEnter}) {
                 onBlur={() => setIsFocused(false)}
                 onKeyDown={handleKeyDown}
                 spellCheck="false"  
+                ref={inputRef}
+            />
+            <input className={`shadow search-bar ${!isFocused ? "center":""}`}
+                placeholder={lower}
+                readOnly={true}
             />
             <img src={x} className="x" alt="x" onClick={handleX} />
             {results.length > 0 && (
