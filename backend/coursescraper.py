eebf7de07b4be03c6710a6b7858f56078c1f0438
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2024-02-13 11:13:07 -0500

    testing1

--- backend/coursescraper.py
+++ backend/coursescraper.py
@@ -126,7 +126,7 @@ async def parse_course_info(url, classroom_info):
 # cluster organization and database management determined
 def upload_to_mongo(dic, term):
     load_dotenv() # loading .env file
-    uri = os.environ.get('MONGO_URL1') # fetching URI string
+    uri = os.environ.get('MONGO_URL') # fetching URI string
     client = MongoClient(uri, server_api=ServerApi('1')) 
     try: # send a ping to confirm a successful connection
         client.admin.command('ping')
@@ -175,14 +175,14 @@ async def main():
 
 if __name__ == "__main__":
     test_mongo_insert_and_read()
-    # start_time = time.perf_counter() # establishing start time for benchmarking
+    start_time = time.perf_counter() # establishing start time for benchmarking
 
-    # asyncio.run(main())# <-- where the magic happens
+    asyncio.run(main())# <-- where the magic happens
 
-    # end_time = time.perf_counter() # end time
+    end_time = time.perf_counter() # end time
 
-    # # calculating and formatting elapsed time
-    # elapsed_time_seconds = end_time - start_time 
-    # minutes = int(elapsed_time_seconds // 60)
-    # seconds = int(elapsed_time_seconds % 60)
-    # print(f"The function took {minutes} minutes and {seconds} seconds to complete.")
+    # calculating and formatting elapsed time
+    elapsed_time_seconds = end_time - start_time 
+    minutes = int(elapsed_time_seconds // 60)
+    seconds = int(elapsed_time_seconds % 60)
+    print(f"The function took {minutes} minutes and {seconds} seconds to complete.")
