e657a95431a9631a25ee9d80eb7e24fd2ad008d4
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2024-02-18 16:52:12 -0500

    added offline support for dev

--- frontend/src/components/Classroom/Classroom.jsx
+++ frontend/src/components/Classroom/Classroom.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Classroom.css';
 // import '../../App.css';  
 
 function Classroom({ name, roomid }) {
+    const [image, setImage] = useState("")
+
+    useEffect(()=>{
+        setImage("");
+    },[name])
+
     if(name === "none" || !name){
         return "";
     }
+
+
+
+    setTimeout(() => {
+        setImage("https://www.jmzarchitects.com/wp-content/uploads/2020/03/17112901-2-scaled.jpg");
+    }, 1000);
+
     return (
         <div className='classroom-component'>
-            <div className="image shimmer">
+            <div className={`image ${image === "" ? "shimmer": ""}`}>
+                {!image == "" ? 
+                    <img src={image}></img>
+                : ""}
             </div>
             <div className="classroom-info">
                 <h1>{name}</h1>
--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -7,32 +7,39 @@ import SearchBar from '../components/SearchBar/SearchBar';
 import useAuth from '../hooks/useAuth';
 import Classroom from '../components/Classroom/Classroom';
 
+import dummyData from '../dummyData.js'
+
 import axios from 'axios';
 
+const offline = true;
 
-function Room(){
+function Room() {
     let { roomid } = useParams();
-    let navigate =  useNavigate();
+    let navigate = useNavigate();
     const [rooms, setRooms] = useState(null);
     const { isAuthenticated, logout } = useAuth();
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    const [freeQuery, setFreeQuery] = useState({
+    const [contentState, setContentState] = useState("empty")
+
+    const [results, setResults] = useState([]);
+    const [query, setQuery] = useState({
         'M': [],
         'T': [],
         'W': [],
         'R': [],
         'F': [],
     });
+    const [noquery, setNoQuery] = useState(true);
 
     console.log("roomid: ", roomid);
     useEffect(() => {
-        if(isAuthenticated === null){
+        if (isAuthenticated === null) {
             console.log("isAuthenticated null");
             return;
         }
-        if(!isAuthenticated){
+        if (!isAuthenticated) {
             console.log("not authenticated");
             // navigate('/');
         }
@@ -42,16 +49,23 @@ function Room(){
 
     useEffect(() => {
         const fetchRooms = async () => {
+            // -- DANGER ZONE - CODE SHOULD NOT TOUCH DEPLOYMENT BRANCH
+            if(offline){
+                console.log("Continuing with OFFLINE development");
+                setRooms(dummyData['rooms']);
+                return;
+            }
+            // --------------------------------------------------
             try {
                 const response = await fetch(`/getrooms`);
                 const responseBody = await response.text(); // First, always read the response as text
-    
+
                 if (!response.ok) {
                     // Log the error if the response status is not OK
                     console.error("Error fetching data:", responseBody);
                     return;
                 }
-    
+
                 let rooms;
                 try {
                     rooms = JSON.parse(responseBody); // Then, parse the text as JSON
@@ -61,67 +75,147 @@ function Room(){
                     console.log("Raw response:", responseBody);
                     return;
                 }
-    
+
                 setRooms(rooms);
                 console.log(rooms);
-            } catch (networkError) {
-                console.error("Network error:", networkError);
+            } catch (error) {
+                console.error("Error:", error);
+
             }
         };
-    
+
         fetchRooms();
-    }, [roomid, isAuthenticated]);
+    }, []);
+
+    const fetchData = async (id) => {
+        setLoading(true);
+        setData(null);
+        try {
+            const response = await fetch(`/getroom/${id}`);
+            const data = await response.json();
+            setData(data);
+            console.log(data);
+        } catch (error) {
+            console.error("Error fetching data: ", error);
+            // -- DANGER ZONE - CODE SHOULD NOT TOUCH DEPLOYMENT BRANCH
+            console.log("Continuing with OFFLINE development");
+            setData(dummyData['room'])
+            // --------------------------------------------------------
+        } finally {
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            setData(null);
-            try {
-                const response = await fetch(`/getroom/${roomid}`);
-                const data = await response.json();
-                setData(data);
-                console.log(data);
-            } catch (error) {
-                console.error("Error fetching data: ", error);
-            } finally{
-                setLoading(false);
-            }
-        };
-
-        fetchData();
+        fetchData(roomid);
     }, [roomid, isAuthenticated]);
 
     const fetchFreeRooms = async () => {
+        setContentState("calendarSearch")
         try {
-          const response = await axios.post('/free', {freeQuery});
-          const roomNames = response.data;
-          console.log(roomNames); // Process the response as needed
+            setLoading(true)
+            const response = await axios.post('/free', { query });
+            const roomNames = response.data;
+            setResults(roomNames);
+            console.log(`names:${roomNames}`); // Process the response as needed
+            console.log(roomNames); // Process the response as needed
         } catch (error) {
-          console.error('Error fetching free rooms:', error);
-          // Handle error
+            console.error('Error fetching free rooms:', error);
+            // -- DANGER ZONE - CODE SHOULD NOT TOUCH DEPLOYMENT BRANCH
+            console.log("Continuing with OFFLINE development");
+            setResults(dummyData['freerooms']);
+            // -------------------------------------------------------
+            // Handle error
+        } finally {
+            setLoading(false);
         }
-      };
+    };
 
-    // useState(() => {
-    //     fetchFreeRooms();
-    // }, [freeQuery]);
+    const addQuery = (key, newValue) => {
+        setNoQuery(false);
+        setQuery(prev => {
+            // Create a list that includes all existing timeslots plus the new one.
+            const allSlots = [...prev[key], newValue];
 
+            // Sort timeslots by start time.
+            allSlots.sort((a, b) => a.start_time - b.start_time);
 
-    function changeURL2(option){
-        navigate(`/room/${option}`,{ replace: true });
+            // Merge overlapping or consecutive timeslots.
+            const mergedSlots = allSlots.reduce((acc, slot) => {
+                if (acc.length === 0) {
+                    acc.push(slot);
+                } else {
+                    let lastSlot = acc[acc.length - 1];
+                    if (lastSlot.end_time >= slot.start_time) {
+                        // If the current slot overlaps or is consecutive with the last slot in acc, merge them.
+                        lastSlot.end_time = Math.max(lastSlot.end_time, slot.end_time);
+                    } else {
+                        // If not overlapping or consecutive, just add the slot to acc.
+                        acc.push(slot);
+                    }
+                }
+                return acc;
+            }, []);
+
+            // Return updated state.
+            return { ...prev, [key]: mergedSlots };
+        });
+    };
+
+    const removeQuery = (key, value) => {
+        setNoQuery(true); //failsafe, useEffect checks before query anyways
+        setQuery(prev => {
+            const existing = prev[key];
+            if (existing === undefined) {
+                // If the key does not exist, return the previous state unchanged.
+                return prev;
+            } else {
+                // Filter the array to remove the specified value.
+                const filtered = existing.filter(v => v !== value);
+                // Always return the object with the key, even if the array is empty.
+                return { ...prev, [key]: filtered };
+            }
+        });
+        // Check if all keys in the query object have empty arrays and update `noQuery` accordingly.
     }
 
-    return(
+    useEffect(() => {
+        Object.keys(query).every(key => query[key].length === 0) ? setNoQuery(true) : setNoQuery(false);
+        if (noquery === true) {
+            setContentState("empty");
+        }
+        console.log(`noquery: ${noquery}`);
+        setResults([]);
+        console.log("query: ", query);
+        if (!noquery) {
+            fetchFreeRooms();
+        }
+    }, [query])
+
+
+    function changeURL2(option) {
+        navigate(`/room/${option}`, { replace: true });
+    }
+
+    return (
         <div className="room">
-            <Header />  
+            <Header />
             <div className="content-container">
                 <div className="calendar-container">
                     <div className="left">
-                        <SearchBar data={rooms} onEnter={changeURL2} room={roomid}/>
-                        <Classroom name={roomid} roomid={roomid}/>
+                        <SearchBar data={rooms} onEnter={changeURL2} room={roomid} />
+                        <Classroom name={roomid} roomid={roomid} />
+                        {contentState === "calendarSearch" ? loading ? "" : <h1>{results.length} results</h1> : ""}
+                        <ul className="time-results">
+                            {
+                                results.map((result, index) => {
+                                    return <li key={index} value={result} onMouseOver={() => { }}>{result.toLowerCase()}</li>
+                                })
+                            }
+                        </ul>
                     </div>
                     <div className="right">
-                        <Calendar className={roomid} data={data} isloading={loading} setFreeQuery={setFreeQuery}/>
+                        <Calendar className={roomid} data={data} isloading={loading} addQuery={addQuery} removeQuery={removeQuery} query={query} />
                     </div>
                 </div>
             </div>
