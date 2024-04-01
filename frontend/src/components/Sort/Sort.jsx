5bbd99cd7555b427a359ca99ff265d8bb92e2674
AZ0228 <jbliu88@gmail.com>
2024-04-01 10:18:34 -0400

    styling changes

--- frontend/src/components/Sort/Sort.jsx
+++ frontend/src/components/Sort/Sort.jsx
@@ -13,20 +13,28 @@ import ChevronUp from '../../assets/Icons/sort/ChevronUp.svg';
 function Sort(){
     const [selected, setSelected] = useState(null);
 
+    const handleSelect = (select) => {
+        if(selected === select){
+            setSelected(null);
+        } else {
+            setSelected(select);
+        }
+    };  
+
 
     return (
         <div className="sort-row">
-            <div className={`tags ${selected === 'tags' ? "selected": ""}`}>
+            <div className={`tags ${selected === 'tags' ? "selected": ""}`} onClick={()=>{handleSelect('tags')}}>
                 <img src={selected === 'tags' ? TagsSelected : Tags} alt="" />
                 <p>Tags</p>
                 <img src={selected === 'tags' ? ChevronUp : ChevronDown} alt="" />
             </div>
-            <div className={`sort-by ${selected === 'sort' ? "selected" : ""}`}>
+            <div className={`sort-by ${selected === 'sort' ? "selected" : ""}`} onClick={()=>{handleSelect('sort')}}>
                 <img src={selected === 'sort' ? SortBySelected : SortBy} alt="" />
-                <p>Sort by</p>
+                <p>Sort</p>
                 <img src={selected === 'sort' ? ChevronUp : ChevronDown} alt="" />
             </div>
-            <div className={`more ${selected === 'sort' ? "selected" : ""}`}>
+            <div className={`more ${selected === 'more' ? "selected" : ""}`} onClick={()=>{handleSelect('more')}}>
                 <img src={selected === 'more' ? MoreSelected : More} alt="" />
                 <p>More</p>
                 <img src={selected === 'more' ? ChevronUp : ChevronDown} alt="" />
