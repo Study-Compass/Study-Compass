e94e4c8ae4b475d2ad1e9c2de43f6dfae550d4f8
Nicole <linn4@rpi.edu>
2024-06-14 14:23:45 -0400

    SCRUM-25 created jsx and css for settings page and made a route to the page

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(dataRoutes);
 app.use(cors());
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL_LOCAL);
 mongoose.connection.on('connected', () => {
     console.log('Mongoose connected to DB.');
     mongoConnection = true;
---
+++ frontend/src/pages/Settings/Settings.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import Header from '../../components/Header/Header';
+import './Settings.css';
+
+
+function Settings(){
+
+    return(
+        <div className='settings'>
+            <Header />
+            <div >
+
+            </div>
+        </div>
+    );
+}
+
+export default Settings;
