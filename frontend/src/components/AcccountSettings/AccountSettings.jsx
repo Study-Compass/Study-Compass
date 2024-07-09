76adc9e530162539cb603970a008add681ad6164
Nicole <linn4@rpi.edu>
2024-07-09 13:50:35 -0400

    SCRUM-25 styling

---
+++ frontend/src/components/AcccountSettings/AccountSettings.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+import './AccountSettings.css';
+import rightarrow from '../../assets/Icons/RightArrow.svg';
+
+function AccountSettings({ settingsRightSide, width, handleBackClick, userInfo }) {
+
+
+    return (
+        <div className={`settings-right ${settingsRightSide ? "active" : "not-active"}`}>
+            <div className='header'>
+                <h1>Account Settings</h1>
+                {width <= 700 && settingsRightSide && (
+                    <button className='back-arrow' onClick={handleBackClick}>
+                        <img src={rightarrow} alt="Back Arrow" style={{ transform: 'rotate(180deg)' }} />
+                    </button>
+                )}
+            </div>
+
+            <div className='profile'>
+
+                <h2>profile settings</h2>
+                <hr />
+
+                <div className='name-settings'>
+                    <img src={userInfo.picture} alt="" />
+
+
+                    <div className='input-name'>
+                        <h3>name:</h3>
+                        <input type="James Liu" />
+                    </div>
+
+                </div>
+
+                <h2>security settings</h2>
+                <hr />
+                <div className='user-container'>
+                    <div className='user'>
+                        <h3>{userInfo.username}</h3>
+                        <p>@id</p>
+                    </div>
+                    <button>change usename</button>
+
+                </div>
+                <div className='user-container'>
+                    <div className='email'>
+                        <h3>email</h3>
+                        <p>{userInfo.email}</p>
+                    </div>
+                    <button>change email</button>
+
+                </div>
+                <div className='user-container'>
+                    <div className='password'>
+                        <h3>password</h3>
+                        <p>enabled</p>
+                    </div>
+                    <button>change password</button>
+
+                </div>
+
+                <h2>danger zone</h2>
+                <hr />
+                <div className='user-container'>
+                    <div className='delete'>
+                        <button>
+                            <h3>delete account</h3>
+
+                        </button>
+                        <p>warning: this is a permanent action !</p>
+
+                    </div>
+                </div>
+
+
+
+            </div>
+            <div className='security'>
+
+            </div>
+
+            <div className='danger'>
+
+            </div>
+
+        </div>
+    )
+
+}
+
+export default AccountSettings;
--- frontend/src/pages/Settings/Settings.jsx
+++ frontend/src/pages/Settings/Settings.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header/Header';
+import AccountSettings from '../../components/AcccountSettings/AccountSettings.jsx';
 import './Settings.css';
 import pfp from '../../assets/defaultAvatar.svg';
 import preferences from '../../assets/Icons/Preferences.svg';
@@ -93,87 +94,14 @@ function Settings() {
 
                         </div>
                     </div>
+                    <AccountSettings settingsRightSide={settingsRightSide} 
+                        width={width} 
+                        handleBackClick={handleArrowClick}
+                        userInfo={userInfo}
+                    />
 
 
-
-                    <div className={`settings-right ${settingsRightSide ? "active" : "not-active"}`}>
-                        <div className='header'>
-                            <h1>Account Settings</h1>
-                            {width <= 700 && settingsRightSide && (
-                                <button className='back-arrow' onClick={handleBackClick}>
-                                    <img src={rightarrow} alt="Back Arrow" style={{ transform: 'rotate(180deg)' }} />
-                                </button>
-                            )}
-                        </div>
-
-                        <div className='profile'>
-
-                            <h2>profile settings</h2>
-                            <hr />
-
-                            <div className='name-settings'>
-                                <img src={pfp} alt="" />
-
-
-                                <div className='input-name'>
-                                    <h3>name:</h3>
-                                    <input type="James Liu" />
-                                </div>
-
-                            </div>
-
-                            <h2>security settings</h2>
-                            <hr />
-                            <div className='user-container'>
-                                <div className='user'>
-                                    <h3>{userInfo.username}</h3>
-                                    <p>@id</p>
-                                </div>
-                                <button>change usename</button>
-
-                            </div>
-                            <div className='user-container'>
-                                <div className='email'>
-                                    <h3>email</h3>
-                                    <p>{userInfo.email}</p>
-                                </div>
-                                <button>change email</button>
-
-                            </div>
-                            <div className='user-container'>
-                                <div className='password'>
-                                    <h3>password</h3>
-                                    <p>enabled</p>
-                                </div>
-                                <button>change password</button>
-
-                            </div>
-
-                            <h2>danger zone</h2>
-                            <hr />
-                            <div className='user-container'>
-                                <div className='delete'>
-                                    <button>
-                                        <h3>delete account</h3>
-
-                                    </button>
-                                    <p>warning: this is a permanent action !</p>
-
-                                </div>
-                            </div>
-
-
-
-                        </div>
-                        <div className='security'>
-
-                        </div>
-
-                        <div className='danger'>
-
-                        </div>
-
-                    </div>
+                    
 
 
 
