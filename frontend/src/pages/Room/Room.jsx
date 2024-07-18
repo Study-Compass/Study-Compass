e07d9c37fe03d8294faefb1987d2830ce6facab5
AZ0228 <jbliu88@gmail.com>
2024-07-17 23:16:13 -0400

    SCRUM-63 updated routing logic

--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -271,16 +271,16 @@ function Room() {
             <div className="content-container">
                 <div className="calendar-container">
                     <div className={width < 800 ? "left-mobile" : "left"}>
-                        {/* {contentState === "empty" && !isClicked ?  */}
+                        {contentState === "empty" && !isClicked ? 
                             <Recommended 
                                 id={"65dd0786d6b91fde155c0097"}
                                 debouncedFetchData={debouncedFetchData}
-                                changeURLHelper={changeURL}
+                                changeURLHelper={changeURL2}
                                 findNext={findNext}
                                 contentState={contentState}
                                 setContentState={setContentState}
                             />
-                         {/* : ""} */}
+                        : ""}
                         <SearchBar data={rooms} onEnter={changeURL2} room={contentState === "classroom" || contentState === "calendarSearchResult" ? roomName : searchQuery } onX={onX} onSearch={onSearch} query={searchQuery} onClick={onClickSearch}/>
                         {contentState === "classroom" || contentState === "calendarSearchResult"  ? <Classroom  
                             room={room} 
