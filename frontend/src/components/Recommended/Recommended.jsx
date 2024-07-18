ca32383620bea6bc8c4bb8c538150088bcc162ee
AZ0228 <jbliu88@gmail.com>
2024-07-17 23:10:01 -0400

    SCRUM-63 added Result component, also added minor ui changes

--- frontend/src/components/Recommended/Recommended.jsx
+++ frontend/src/components/Recommended/Recommended.jsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Recommended.css";
+import Result from "../Results/Result/Result";
+import { useCache } from '../../CacheContext.js';
+import { useNavigate } from "react-router-dom";
+import { useError } from '../../ErrorContext.js';
+import Loader from "../Loader/Loader.jsx";
 
-function Recommended() {
+function Recommended({id, debouncedFetchData, changeURLHelper, findNext, setContentState}) {
+    const [room, setRoom] = useState(null);
+
+    const { getRoom } = useCache();
+    const { newError } = useError();
+    
+    const navigate = useNavigate();
+
+    const fetchDataHelper = async (id) => {
+        try{
+            const data = await getRoom(id);
+            setRoom(data);
+            console.log(data);
+        } catch (error){
+            console.log(error);
+            newError(error, navigate);
+        }
+    };
+
+    useEffect(() => {
+        fetchDataHelper(id);
+    }, [id]);
+
+    if(!room){
+        return(
+            <div className="recommended">        
+                <Loader/>
+            </div>
+        )
+    }
+    
+    
     return(
         <div className="recommended">
             <p>recommended for you</p>
-            <p>picture</p>
+            <Result 
+                result={room} 
+                attributes={room.room.attributes}
+                debouncedFetchData={debouncedFetchData} 
+                changeURL={changeURLHelper}
+                findNext={findNext}
+                contentState={"nameSearch"}
+            />
         </div>
     )
 }
