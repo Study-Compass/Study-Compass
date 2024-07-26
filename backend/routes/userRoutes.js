8d142dd569944408cf698d5a1f0ff09acebe9bd8
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2024-07-26 13:36:54 -0400

    SCRUM-2 added last onboarding step as well as ui changes, non-responsive as of yet

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -19,9 +19,11 @@ router.post("/update-user", verifyToken, async (req, res) =>{
         user.onboarded = onboarded ? onboarded : user.onboarded;
         
         await user.save();
-        console.log(`POST: /update-user ${req.user.userId} successful`)
+        console.log(`POST: /update-user ${req.user.userId} successful`);
+        return res.status(200).json({ success: true, message: 'User updated successfully' });
     } catch(error){
-
+        console.log(`POST: /update-user ${req.user.userId} failed`)
+        return res.status(500).json({ success: false, message: 'Internal server error' });
     }
 });
 
