7154ea7b7cd9d4fc3c7736939195ea3d8e6a9e3c
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2024-09-21 21:18:01 -0400

    SCRUM-54 fixing some layout bugs

--- frontend/src/pages/Layout/Layout.jsx
+++ frontend/src/pages/Layout/Layout.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom'; // Allows for nested routes to be rendered within this layout
 import Banner from '../../components/Banner/Banner'; // Import your Banner component
 
 function Layout() {
   const [visible, setVisible] = useState(false);
 
+  const [viewport, setViewport] = useState("100vh");
+  useEffect(() => {
+      let height = window.innerHeight;
+      setViewport(height + 'px');
+      //add listener
+  },[]);
+  
   return (
-    <div>
+    <div style={{height:viewport}}>
       {/* The Banner is rendered here and will appear across all pages */}
       <Banner visible={visible} setVisible={setVisible} bannerType="default" />
       
       {/* This will render the content of the page (children) */}
       <main>
-        <div className="out" style={{height:visible ? `calc(100vh - 20px)` : `100vh`}}>
+        <div className="out" >
 
           <Outlet />
         </div>
