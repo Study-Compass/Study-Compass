5a65eacb8ee67893daf59ea0e994a9b370f2d449
himekatie <useforanything9@gmail.com>
2024-10-18 16:40:31 -0400

    SCRUM-112 added array of mock history objects

--- frontend/src/components/ProfileCard/StudyHistory/HistoryEntry/HistoryEntry.jsx
+++ frontend/src/components/ProfileCard/StudyHistory/HistoryEntry/HistoryEntry.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import "./HistoryEntry.scss";
 import pfp from '../../../../assets/defaultAvatar.svg'
 
-const HistoryEntry = ({})  => {
+const HistoryEntry = ({mockHistory})  => {
 
+    
     return(
         <div className = "historyentry">
-                <h3>russel sage laboratories 2715
+                <h3>{mockHistory.roomName}
                     <h6><img src={pfp} alt="" /></h6>
                 </h3>
-                <h4>Friday 10/8
-                <h5>3:00 - 5:00</h5>
+                <h4>{mockHistory.day}
+                <h5>{mockHistory.time}</h5>
                 </h4>
         </div>
     );
--- frontend/src/components/ProfileCard/StudyHistory/StudyHistory.jsx
+++ frontend/src/components/ProfileCard/StudyHistory/StudyHistory.jsx
@@ -17,6 +17,31 @@ function StudyHistory({userInfo}){
         setIsOpen(!isOpen);
     };
 
+    const mockHistory = [{
+        roomName: "russel sage laboratories 2715", day: "Friday 10/8", time: "3:00 - 6:00"
+    },
+    {
+        roomName: "russel sage laboratories 2716", day: "Friday 10/9", time: "3:00 - 6:00"
+    },
+    {
+        roomName: "russel sage laboratories 2717", day: "Friday 10/10", time: "6:00 - 8:00"
+    },
+    {
+        roomName: "russel sage laboratories 2718", day: "Friday 10/11", time: "3:00 - 5:00"
+    },
+    {
+        roomName: "dcc 318", day: "Friday 10/12", time: "3:00 - 5:00"
+    },
+    {
+        roomName: "dcc 308", day: "Friday 10/13", time: "3:00 - 6:00"
+    },
+    ]
+    
+
+
+
+
+
     return (
         <div className={`study-history ${isOpen ? 'open' : ''}`}>
             <div className="box-header">
@@ -28,7 +53,12 @@ function StudyHistory({userInfo}){
                     </button>
                 </div>
                 <div className={`history-content ${isOpen ? 'open' : ''}`}>
-                    <HistoryEntry></HistoryEntry>
+                    <HistoryEntry mockHistory={mockHistory[0]}/>
+                    <HistoryEntry mockHistory={mockHistory[1]}/>
+                    <HistoryEntry mockHistory={mockHistory[2]}/>
+                    <HistoryEntry mockHistory={mockHistory[3]}/>
+                    <HistoryEntry mockHistory={mockHistory[4]}/>
+                    <HistoryEntry mockHistory={mockHistory[5]}/>
                 </div>
             </div>
         </div>
