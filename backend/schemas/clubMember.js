d829206b9675e23c39e9f0ef0fe400253bc120af
Phoenix Keck <Phoenixrocks995@gmail.com>
2024-10-25 17:23:25 -0400

    SCRUM-125 Club, Members, Followers Schema Created

---
+++ backend/schemas/clubMember.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const memberSchema = new Schema({
+    club_id: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Club'
+    },
+    user_id: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    status: {
+        //Ranking in club
+        type: Number, 
+        required:true,
+    }
+});
+
+
+const ClubMember = mongoose.model('Ratings', memberSchema, 'ratings');
+
+module.exports = ClubMember;
