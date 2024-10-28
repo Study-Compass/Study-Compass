2ea9a3fa9028f7b7d58bf09f70b34adcf6d2a938
AZ0228 <jbliu88@gmail.com>
2024-10-27 23:54:22 -0400

    SCRUM-12 implementing event structure

---
+++ frontend/src/components/EventsViewer/EventsGrid/EventsColumn/FullEvent/FullEvent.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './FullEvent.scss';
+import {Icon} from '@iconify-icon/react';  
+import StarGradient from '../../../../../assets/StarGradient.png'
+
+function FullEvent({ event }){
+    const navigate = useNavigate();
+
+    const handleEventClick = () => {
+        navigate(`/create-event`);
+    }
+    const date = new Date(event.date);
+
+    return(
+        <div className="full-event">
+            <div className="image">
+                <img src={event.image} alt="" />
+            </div>
+            <div className="content">
+            <h1>{event.name}</h1>
+                {/* <p>{event.location }</p> */}
+                {/* display date in day of the week, month/day */}
+                <p>{date.toLocaleString('default', {weekday: 'long'})}, {date.toLocaleString('default', {month: 'long'})} {date.getDate()}</p>
+                {/* time */}
+                <p>{date.toLocaleString('default', {hour: 'numeric', minute: 'numeric', hour12: true})}</p>
+
+            </div>
+            <img src={StarGradient} alt="" className="gradient" />
+        </div>
+    );
+
+}
+
+export default FullEvent;
