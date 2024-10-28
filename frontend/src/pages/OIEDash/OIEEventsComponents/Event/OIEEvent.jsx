8d87721c29f851a3288bb16fd358b63ac3821a7f
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2024-10-28 16:00:12 -0400

    SCRUM-146 restructuring event for admin-side

--- frontend/src/pages/OIEDash/OIEEventsComponents/Event/OIEEvent.jsx
+++ frontend/src/pages/OIEDash/OIEEventsComponents/Event/OIEEvent.jsx
@@ -3,6 +3,8 @@ import './OIEEvent.scss';
 import { useNavigate } from 'react-router-dom';
 import Popup from '../../../../components/Popup/Popup';
 import OIEFullEvent from '../FullEvent/OIEFullEvent';
+import { Icon } from '@iconify-icon/react/dist/iconify.mjs';
+import defaultAvatar from '../../../../assets/defaultAvatar.svg';
 
 function OIEEvent({event}){
     const [popupOpen, setPopupOpen] = useState(false);
@@ -20,19 +22,31 @@ function OIEEvent({event}){
     
 
     return(
-        <div className="event-component" onClick={() => handleEventClick(event)}>
+        <div className="oie-event-component" >
             <Popup isOpen={popupOpen} onClose={onPopupClose} customClassName={"wide-content"}>
                 <OIEFullEvent event={event}/>
             </Popup>
-            {event.image && <img src={event.image} alt="" />}
             <div className="info">
                 <h1>{event.name}</h1>
                 {/* <p>{event.location }</p> */}
                 {/* display date in day of the week, month/day */}
-                <p>{date.toLocaleString('default', {weekday: 'long'})}, {date.toLocaleString('default', {month: 'long'})} {date.getDate()}</p>
+                <div className="row">
+                    <img src={event.user_id.image ? event.user_id.image : defaultAvatar} alt="" />
+                    <p className="user-name">{event.user_id.name}</p>
+                </div>
+                <div className="row">
+                    <Icon icon="heroicons:calendar-16-solid" />
+                    <p>{date.toLocaleString('default', {weekday: 'long'})} {date.toLocaleString('default', {month: 'numeric'})}/{date.getDate()}</p>
+                    <Icon icon="fluent:location-28-filled" />
+                    <p>{event.location}</p>
+                </div>
                 {/* time */}
-                <p>{date.toLocaleString('default', {hour: 'numeric', minute: 'numeric', hour12: true})}</p>
+                {/* <p>{date.toLocaleString('default', {hour: 'numeric', minute: 'numeric', hour12: true})}</p> */}
             </div>
+            <button className="button" onClick={() => handleEventClick(event)}>
+                <Icon icon="material-symbols:expand-content-rounded" />
+                <p>details</p>
+            </button>
         </div>
     );
 
