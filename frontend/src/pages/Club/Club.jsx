ec9aad07423c393d8b4677e6457d742065de866e
Nicole <linn4@rpi.edu>
2024-11-08 17:46:16 -0500

    SCRUM-75 editing the header of club page

--- frontend/src/pages/Club/Club.jsx
+++ frontend/src/pages/Club/Club.jsx
@@ -1,18 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Club.scss';
+import Header from '../../components/Header/Header';
+// import rpiLogo from "./assets/Icon/rpiLogo.svg";
 
 
-const Club = ({}) => {
+const Club = () => {
+    return (
+        <div className="club-page">
+            <Header />
+            <div className="top-header-box">
+                <div className="club-logo">
+                    <img src="rpiLogo" alt=""/>
+                </div>
+                <div className="club-info">
 
-    return(
-        <div>
+                    <div className="club-header">
+                        <h2 className="name">Club Name</h2>
+                        <span className="status">Union Recognized</span>
+                    </div>
 
+                    <p className="description">
+                        description of the club blah blah blah blah blah
+                    </p>
+                    <div className="actions">
+                        <button>Join</button>
+                        <button>Follow</button>
+                    </div>
+                    <div className="stats">
+                        <span>250 followers • 50 members</span>
+                    </div>
+                </div>
+            </div>
+
+            
         </div>
-
     );
 };
-
-
-
 
 export default Club;
