b09b139df6e30572b6a438bdb7a349ce0f530546
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2024-11-23 19:24:42 -0500

    SCRUM-125 finsihed club creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,6 +22,7 @@ import CreateClub from './pages/CreateClub/CreateClub';
 import NewBadge from './pages/NewBadge/NewBadge';
 import ClubDash from './pages/ClubDash/ClubDash';
 import NewBadge from './pages/NewBadge/NewBadge';
+import CreateClub from './pages/CreateClub/CreateClub';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
 import { CacheProvider } from './CacheContext';
@@ -131,6 +132,7 @@ function App() {
                                             <Route path="/club-dashboard" element={<ClubDash/>}/>
                                             
                                             <Route path="/new-badge/:hash" element={<NewBadge/>}/>
+                                            <Route path='/create-club' element={<CreateClub/>}/>
                                         </Route>
                                     </Routes>
                                     </ProfileCreationProvider>
