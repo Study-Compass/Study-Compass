bb8774e38c5effee723c7b90450ff76b3d7ee1d3
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2024-12-02 15:54:06 -0500

    SCRUM-164 finished new badge routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import Admin  from './pages/Admin/Admin';
 import OIEDash from './pages/OIEDash/OIEDash';
 import NewBadge from './pages/NewBadge/NewBadge';
 import CreateClub from './pages/CreateClub/CreateClub';
+import NewBadge from './pages/NewBadge/NewBadge';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
 import { CacheProvider } from './CacheContext';
@@ -123,6 +124,7 @@ function App() {
                                             <Route path="/oie-dashboard" element={<OIEDash/>}/>
                                             <Route path="/new-badge/:hash" element={<NewBadge/>}/>
                                             <Route path='/create-club' element={<CreateClub/>}/>
+                                            <Route path="/new-badge" element={<NewBadge/>}/>
                                         </Route>
                                     </Routes>
                                     </ProfileCreationProvider>
