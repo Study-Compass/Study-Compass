6f3daa21a9c23e62575f6dbc94de9564e5c864a0
Phoenix Keck <Phoenixrocks995@gmail.com>
2024-12-10 11:49:37 -0500

    SCRUM-158 Added Monitor and Logging Middleware

--- backend/middlewares/monitoring.js
+++ backend/middlewares/monitoring.js
@@ -1,3 +1,37 @@
 //Monitor and Logger
-const express = require('express');
+const API = require('../models/api.js');
 const morgan = require('morgan');
+
+//Logging HTTP Requests
+//CAN CHANGE FORMATTING LATER
+const logRequests=morgan('combined')
+
+//Monitoring and Tracking The API-KEY usage
+const monitorUsage = async (req, res, next)=>{
+    const {authorization_key, api_key} = req.headers;
+
+    //If authorization/api missing -  continue
+    if (!authorization_key || !api_key){
+        return next();
+    }
+    try{
+        const apiEntry = await API.findOne({authorization_key, api_key});
+        if (apiEntry){
+            //If Key valid, count usage
+            apiEntry.usageCount +=1;
+            await apiEntry.save();
+
+            //Can delete later
+            console.log(`API Key used: ${api_key}, Usage Count: ${apiEntry.usageCount}`);
+        }
+
+        next();
+    } catch(error){
+        //Change this error
+        console.error ('Error verifying or updating API key usage:', error);
+        //Continue 
+        return next();
+    }
+};
+
+module.exports = {logRequests, monitorUsage};
