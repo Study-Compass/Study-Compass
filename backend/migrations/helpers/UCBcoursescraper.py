8ebfd42898734d17db20a8ac5be1515ad55d9182
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2025-01-07 18:35:20 -0800

    SCRUM-165 finished very barebones scraper

--- backend/migrations/helpers/UCBcoursescraper.py
+++ backend/migrations/helpers/UCBcoursescraper.py
@@ -32,21 +32,21 @@ async def get_course_ids(term):
 
 
 def getMeetingDays(meetings):
-    days=""
+    days=[]
     if meetings['meetsSaturday']:
-        days+="Sa"
+        print('class meets on saturday')
     if meetings['meetsMonday']:
-        days+="Mo"
+        days.append("M")
     if meetings['meetsTuesday']:
-        days+="Tu"
+        days.append("T")
     if meetings['meetsWednesday']:
-        days+="We"
+        days.append("W")
     if meetings['meetsThursday']:
-        days+="Th"
+        days.append("R")
     if meetings['meetsFriday']:
-        days+="Fr"
+        days.append("F")
     if meetings['meetsSunday']:
-        days+="Su"
+        print('class meets on sunday')
     return days
 
 def checkMultipleMeetings(meetings):
@@ -59,10 +59,32 @@ def checkMultipleMeetings(meetings):
             return False
     return True
         
-    
+'''
+example
+    "AFRICAM 15B": {
+        "name": "2025 Spring AFRICAM 15B 001 REC 001",
+        "location": {
+            "code": "DWINB7",
+            "description": "Dwinelle B7"
+        },
+        "time": [
+            "14:00:00",
+            "15:59:00"
+        ],
+        "days": "TuTh"
+    },
+'''
+
+# function to turn time strings into number of minutes since midnight, rounded to the nearest 15 minutes
+# example: "14:00:00" -> 840
+def timeToMinutes(time):
+    time = datetime.strptime(time, '%H:%M:%S')
+    # round to the nearest 15 minutes
+    return time.hour * 60 + time.minute
+
 
 # view-source:https://classes.berkeley.edu/search/class/?f%5B0%5D=im_field_term_name:3153
-async def parse_results(dic, pages=341):
+async def parse_results(dic, pages=1):
     entries = 0
     backoff_factor = 1.0  # Initial backoff factor
     max_retries = 5  # Maximum number of retries for each request
@@ -71,7 +93,7 @@ async def parse_results(dic, pages=341):
         for i in range(pages):
             for attempt in range(max_retries):
                 try:
-                    url = f"https://classes.berkeley.edu/search/class?page={i}&f%5B0%5D=im_field_term_name:3153"
+                    url = f"https://classes.berkeley.edu/search/class?page={i+157}&f%5B0%5D=im_field_term_name:3153"
                     response = await client.get(url)
                     if response.status_code == 429:  # Too many requests
                         raise httpx.HTTPStatusError(message="Rate limit exceeded", request=response.request, response=response)
@@ -88,23 +110,46 @@ async def parse_results(dic, pages=341):
                         if not data_json.get('meetings'):
                             print('no meetings found :(')
                             continue
-                        if data_json['instructionMode']['code'] == 'P':  # Only in-person classes
-                            classroom_info = {
-                                "name": data_json['displayName'],
-                                "location": data_json['meetings'][0]["location"],
-                                "time": [
-                                    data_json['meetings'][0]["startTime"],
-                                    data_json['meetings'][0]["endTime"],
-                                ],
-                                "days": getMeetingDays(data_json['meetings'][0]),
-                            }
-                            dic[name] = classroom_info
+                        if data_json['instructionMode']['code'] == 'P' and data_json['meetings'][0]["location"] != []:  # Only in-person classes
+                            # classroom_info = {
+                            #     "name": data_json['displayName'],
+                            #     "location": data_json['meetings'][0]["location"],
+                            #     "time": [
+                            #         timeToMinutes(data_json['meetings'][0]["startTime"]),
+                            #         timeToMinutes(data_json['meetings'][0]["endTime"]),
+                            #     ],
+                            #     "days": getMeetingDays(data_json['meetings'][0]),
+                            # }
+                            for i in range(len(data_json['meetings'])):
+                                
+                                course_name = data_json['displayName'].split(' ')[2] + ' ' + data_json['displayName'].split(' ')[3]
+                                if type(data_json['meetings'][i]["location"]) == list:
+                                    print('location is a list')
+                                    print(data_json['meetings'][i]["location"])
+                                    continue
+                                location = data_json['meetings'][i]["location"]["description"]
 
-                        if len(data_json['meetings']) > 1:
-                            if not checkMultipleMeetings(data_json['meetings']):
-                                print('multiple meetings not matching :(')
-                                for meeting in data_json['meetings']:
-                                    print(getMeetingDays(meeting))
+                                classroom_info = {
+                                    "class_name" : course_name,
+                                    "start_time" : timeToMinutes(data_json['meetings'][i]["startTime"]),
+                                    "end_time" : timeToMinutes(data_json['meetings'][i]["endTime"]),
+                                }
+                                
+                                if location not in dic:
+                                    dic[location] = {
+                                        "name": location,
+                                        "weekly_schedule": {"M": [], "T": [], "W": [], "R": [], "F": []}
+                                    }
+
+                                for day in getMeetingDays(data_json['meetings'][i]):
+                                    dic[location]["weekly_schedule"][day].append(classroom_info)
+
+
+                            if len(data_json['meetings']) > 1:
+                                if not checkMultipleMeetings(data_json['meetings']):
+                                    print('multiple meetings not matching :(')
+                                    for meeting in data_json['meetings']:
+                                        print(getMeetingDays(meeting))
                     break  # If successful, break out of the retry loop
                 except (httpx.ReadTimeout, httpx.HTTPStatusError) as e:
                     wait = (2 ** attempt) * backoff_factor + random.uniform(0, 1)  # Exponential backoff + jitter
@@ -112,7 +157,7 @@ async def parse_results(dic, pages=341):
                     await asyncio.sleep(wait)
                 except Exception as e:
                     print(f"An unexpected error occurred: {e}")
-                    print('full error: ', e)
+                    print('full error: ', e, 'on line' , e.__traceback__.tb_lineno)
                     break  # Break the loop on unknown errors
 
         # Print the number of entries found
