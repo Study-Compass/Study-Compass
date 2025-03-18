bcf701d507d5b6d5fca7ab2d462c50237aa7af50
himekatie <useforanything9@gmail.com>
2025-03-18 16:47:39 -0400

    SCRUM-191-Club-Meetings added the active meetings slot in the MeetingManagment file and the ongoing part in the ClubEvent

--- frontend/src/pages/ClubDash/ClubEventsComponents/Event/ClubEvent.jsx
+++ frontend/src/pages/ClubDash/ClubEventsComponents/Event/ClubEvent.jsx
@@ -6,7 +6,7 @@ import ClubFullEvent from '../FullEvent/ClubFullEvent';
 import { Icon } from '@iconify-icon/react/dist/iconify.mjs';
 import defaultAvatar from '../../../../assets/defaultAvatar.svg';
 
-function ClubEvent({event}){
+function ClubEvent({event, onGoing}){
     const [popupOpen, setPopupOpen] = useState(false);
     const navigate = useNavigate();
 
@@ -19,6 +19,17 @@ function ClubEvent({event}){
     }
 
     const date = new Date(event.start_time);
+
+
+    const getDifferenceHours = () => {
+        const diffMs = new Date().getTime() - date.getTime(); 
+        return Math.floor(diffMs / (1000 * 60 * 60));
+
+    }
+    const getDifferenceMinutes = () => {
+        const diffMs = new Date().getTime() - date.getTime(); 
+        return Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60)); 
+    }
     
 
     return(
@@ -30,6 +41,12 @@ function ClubEvent({event}){
                 <h1 className="title">{event.name}</h1>
                 {/* <p>{event.location }</p> */}
                 {/* display date in day of the week, month/day */}
+                {onGoing && (
+                    <div className="ongoing-badge">
+                     <span>&#8226;</span>
+                    <p>ongoing for {getDifferenceHours()}:{String(getDifferenceMinutes()).padStart(2, "0")}</p>
+                    </div>
+                )}
                 <div className="row">
                     <Icon icon="heroicons:calendar-16-solid" />
                     <p>{date.toLocaleString('default', {weekday: 'long'})} {date.toLocaleString('default', {month: 'numeric'})}/{date.getDate()}</p>
