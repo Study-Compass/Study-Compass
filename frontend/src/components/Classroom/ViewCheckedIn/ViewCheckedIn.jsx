19d4752b6a1d7e05412246809239337c1c6013e5
GheaCRPI <149848354+GheaCRPI@users.noreply.github.com>
2025-04-07 13:24:59 -0400

    SCRUM-193 add friend button initial

--- frontend/src/components/Classroom/ViewCheckedIn/ViewCheckedIn.jsx
+++ frontend/src/components/Classroom/ViewCheckedIn/ViewCheckedIn.jsx
@@ -1,12 +1,54 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import pfp from '../../../assets/defaultAvatar.svg';
 import Badges from '../../Badges/Badges';
+import Friend from '../../../pages/Friends/Friend/Friend.jsx';
+import { getFriends, sendFriendRequest, updateFriendRequest, getFriendRequests, debounceUserSearch } from '../../../pages/Friends/FriendsHelpers.js';
+import { useNotification } from '../../../NotificationContext.js';
 import './ViewCheckedIn.scss';
 
 
-function ViewCheckedIn({currentUser, users, room, checkIn, checkOut, addFriend}){
+function ViewCheckedIn({currentUser, users, room, checkIn, checkOut}){
     const [error, setError] = useState([]);
+    const [reload, setReload] = useState(0);
+    const [friends, setFriends] = useState([]);
+    const [friendRequests, setFriendRequests] = useState([]);
+    const { addNotification } = useNotification();
+    const handleFriendRequest = async (friendUsername) => {
+        const result = await sendFriendRequest(friendUsername);
+        setReload(reload + 1);
+        if(result === 'Friend request sent'){
+            addNotification({title: 'Friend request sent', message: 'Friend request sent successfully', type: 'success'});
+            return;
+        }
+        if(result === 'User not found'){
+            addNotification({title: 'User not found', message: 'We could not find a user with that username', type: 'error'});
+        } else {
+            addNotification({title: 'Error sending friend request', message: result, type: 'error'});
+        }
+    }
+
+    useEffect(() => {
+            const fetchFriends = async () => {
+                try{
+                    const result = await getFriends();
+                    setFriends(result);
+                } catch (error){
+                    console.error('Error fetching friends:', error);
+                }
+            }
+            const fetchFriendRequests = async () => { 
+                try{
+                    const result = await getFriendRequests();
+                    console.log(result);
+                    setFriendRequests(result);
+                } catch (error){
+                    console.error('Error fetching friend requests:', error);
+                }
+            }
+            fetchFriends();
+            fetchFriendRequests();
+        }, [reload]);
+
     useEffect(() => {
         //populate error array with all false for every user
         setError(new Array(users.length).fill(false));
@@ -17,13 +59,14 @@ function ViewCheckedIn({currentUser, users, room, checkIn, checkOut, addFriend})
         newError[index] = true;
         setError(newError);
     }
+    console.log(currentUser);
     useEffect(() => {
         console.log(error);
     }, [error]);
     return (
         <div className="checked-in-users">
             {
-                <h3>Currently checked in to {room[1]}</h3>
+                <h3>Currently checked in to {room.name}</h3>
             }
             <div className="list-users">
                 {users.map((user, index) => {
@@ -38,20 +81,22 @@ function ViewCheckedIn({currentUser, users, room, checkIn, checkOut, addFriend})
                                     onError={()=> onError(index)}
                                 />
                                 <div className="list-user-right">
-                                    <h2>{user.name}</h2>
+                                    <div className="top-line">
+                                        <h2>{user.name}</h2>
+                                        <div className="checked-in-hours">
+                                            Checked in for x hours
+                                        </div>
+                                    </div>
                                     <h4>@{user.username}</h4>
                                     <Badges badges={user && user.tags} size="9px"/>
                                 </div>
-                                <div className="add-friend">
-
+                                <div className="add-friend" onClick={ handleFriendRequest }>
+                                    add
                                 </div>
                             </div>
-                        
-                        {users.length===1 ? 
-                        <span></span> : <span className="multiple-user"></span>} 
-
+                            
+                            {users.length===1 ? <span></span> : <span className="multiple-user"></span>} 
                         </div>
-                        
                     )
                 })}
             </div>
