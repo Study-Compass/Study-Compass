0da9dbb0b99446b76721841aba61e9f375015cb3
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2025-05-28 09:48:12 -0700

    SCRUM-54 actually fixing friendship bug

--- backend/routes/friendRoutes.js
+++ backend/routes/friendRoutes.js
@@ -105,14 +105,13 @@ router.get('/getFriends', verifyToken, async (req, res) => {
 
 router.get('/friend-requests', verifyToken, async (req, res) => {
     const { Friendship } = getModels(req, 'Friendship');
-    const userId = req.user._id;
+    const userId = req.user.userId;
 
     try {
         const requests = await Friendship.find({
             recipient: userId,
-            status: 'pending'
-        }).populate('requester', 'username');
-
+            status: 'pending'   
+        }).populate('requester');
         res.json({
             success: true,
             message: 'Friend requests found',
